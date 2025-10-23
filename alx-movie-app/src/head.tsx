b74442971da13bc165519e80fc828554b21a4517
CynthiaKaluson <cynthiakalu1974@gmail.com>
2025-10-23 09:05:20 +0100

    feat: add app head to link manifest and PWA meta tags

---
+++ alx-movie-app/src/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+    return (
+        <>
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="theme-color" content="#FFFFFF" />
+            <meta name="mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
+        </>
+    )
+}
